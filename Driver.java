6e5aba26127dc11169e2aeb5c627ecb05229f982
Michael Zhou <michaelzhou20111@hotmail.com>
2023-06-11 07:52:24 -0400

    final update for lab2

--- Driver.java
+++ Driver.java
@@ -7,11 +7,11 @@ public class Driver {
     public static void main(String[] args) {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double[] c1 = { 6, 0, 0, 5 };
-        int[] e1 = {0, 1, 2, 3};
+        double[] c1 = { 6, 5 };
+        int[] e1 = {0, 3};
         Polynomial p1 = new Polynomial(c1, e1);
-        double[] c2 = { 0, -2, 0, 0, -9 };
-        int[] e2 = {0, 1, 2, 3, 4};
+        double[] c2 = { -2, -9 };
+        int[] e2 = {1, 4};
         Polynomial p2 = new Polynomial(c2, e2);
 
         //multiply test
@@ -28,6 +28,20 @@ public class Driver {
         }
         */
         //
+
+        //add test
+        double[] c3 = { 4, 4, 3, 5, 4 };
+        int[] e3 = {0, 1, 2, 3, 7};
+        Polynomial p3 = new Polynomial(c3, e3);
+        double[] c4 = { -4, -4, -3, -5, -4 };
+        int[] e4 = {0, 1, 2, 3, 7};
+        Polynomial p4 = new Polynomial(c4, e4);
+        Polynomial q = p3.add(p4);
+        for (int i = 0; i < q.coefficients.length; i++){
+            System.out.println("coefficients: " + q.coefficients[i] + "exponents: " + q.exponents[i]);
+        }
+
+        // file test
         /* 
         File txt = new File("file.txt");
         try {
@@ -43,6 +57,9 @@ public class Driver {
         }
         */
         Polynomial s = p1.add(p2);
+        for (int i = 0; i < s.coefficients.length; i++){
+            System.out.println("coefficients: " + s.coefficients[i] + "exponents: " + s.exponents[i]);
+        }
         System.out.println("s(0.1) = " + s.evaluate(0.1));
         if (s.hasRoot(1))
             System.out.println("1 is a root of s");
--- Polynomial.java
+++ Polynomial.java
@@ -1,21 +1,21 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class Polynomial{
+public class Polynomial {
     double[] coefficients;
     int[] exponents;
 
-    public Polynomial(){
+    public Polynomial() {
         coefficients = new double[0];
         exponents = new int[0];
     }
 
-    public Polynomial(double[] coefficients_arg, int[] exponents_arg){
+    public Polynomial(double[] coefficients_arg, int[] exponents_arg) {
         coefficients = coefficients_arg;
         exponents = exponents_arg;
     }
 
-    public Polynomial(File f) throws FileNotFoundException{
+    public Polynomial(File f) throws FileNotFoundException {
         Scanner input = new Scanner(f);
         String s = input.next();
         s = s.replace("-", "+-");
@@ -26,73 +26,134 @@ public class Polynomial{
         exponents = new int[len];
 
         int count = 0;
-        for (String str : sp){
+        for (String str : sp) {
             String[] term = str.split("x");
             coefficients[count] = Double.parseDouble(term[0]);
-            if (term.length != 1){
+            if (term.length != 1) {
                 exponents[count] = Integer.parseInt(term[1]);
-            } 
+            }
             count++;
         }
 
         input.close();
     }
 
-    public Polynomial add(Polynomial p){
-        int len = Math.max(p.coefficients.length, coefficients.length);
+    public Polynomial add(Polynomial p) {
+        int len = p.coefficients.length * coefficients.length;
         double[] arr = new double[len];
         int[] arr2 = new int[len];
-        Polynomial q = new Polynomial(arr, arr2);
 
-        for (int i = 0; i < len; i++){
-            if (i < p.coefficients.length) {
-                q.coefficients[i] += p.coefficients[i];
-                q.exponents[i] += p.exponents[i];
+        int count = p.coefficients.length;
+
+        for (int i = 0; i < p.coefficients.length; i++) {
+            arr[i] = p.coefficients[i];
+            arr2[i] = p.exponents[i];
+        }
+
+        for (int i = 0; i < coefficients.length; i++) {
+            if (exponents[i] == 0) {
+                boolean check = true;
+                for (int j = 0; j < p.coefficients.length; j++) {
+                    if (arr2[j] == 0) {
+                        arr[j] += coefficients[i];
+                        check = false;
+                    }
+                }
+                if (check) {
+                    arr2[count] += exponents[i];
+                    arr[count] += coefficients[i];
+                    count++;
+                }
+                continue;
             }
-            if (i < coefficients.length) {
-                q.coefficients[i] += coefficients[i];
-                q.exponents[i] += exponents[i];
+            boolean check = true;
+            for (int j = 0; j < arr2.length; j++) {
+                if (exponents[i] == arr2[j]) {
+                    check = false;
+                    arr[j] += coefficients[i];
+                }
+            }
+            if (check) {
+                arr2[count] += exponents[i];
+                arr[count] += coefficients[i];
+                count++;
             }
         }
+
+        count = 0;
+        for (int i = 0; i < len; i++) {
+            if (arr[i] != 0 && arr2[i] != 0) {
+                count++;
+            }
+        }
+        int q_len = count + 1;
+        double[] q_arr = new double[q_len];
+        int[] q_arr2 = new int[q_len];
+
+        for (int i = 0; i < len; i++) {
+            if (arr2[i] == 0) {
+                if (arr[i] != 0) {
+                    q_arr[q_len - 1] = arr[i];
+                } else {
+                    q_arr = new double[q_len - 1];
+                    q_arr2 = new int[q_len - 1];
+                }
+                break;
+            }
+        }
+
+        count = 0;
+        for (int i = 0; i < len; i++) {
+            if (arr[i] != 0 && arr2[i] != 0) {
+                q_arr[count] = arr[i];
+                q_arr2[count] = arr2[i];
+                count++;
+            }
+        }
+
+        Polynomial q = new Polynomial(q_arr, q_arr2);
 
         return q;
     }
 
-    public double evaluate(double x){
-        if (coefficients.length == 0) return 0.0;
+    public double evaluate(double x) {
+        if (coefficients.length == 0)
+            return 0.0;
         double d = 0;
-        if (x == 0){
-            for (int i = 0; i < exponents.length; i++){
-                if (exponents[i] == 0){
+        if (x == 0) {
+            for (int i = 0; i < exponents.length; i++) {
+                if (exponents[i] == 0) {
                     return coefficients[i];
                 }
             }
             return 0;
         }
-        for(int i = 0; i < coefficients.length; i++){
-            d += coefficients[i]*Math.pow(x, i);
+        for (int i = 0; i < coefficients.length; i++) {
+            d += coefficients[i] * Math.pow(x, exponents[i]);
         }
         return d;
     }
 
-    public boolean hasRoot(double x){
+    public boolean hasRoot(double x) {
         return evaluate(x) == 0;
     }
 
-    public Polynomial multiply(Polynomial p){
+    public Polynomial multiply(Polynomial p) {
         int len = p.coefficients.length * coefficients.length;
         double[] arr = new double[len];
         int[] arr2 = new int[len];
 
         int count = 0;
-        for(int i = 0; i < p.coefficients.length; i++){
-            if (p.coefficients[i] == 0) continue;
-            for(int j = 0; j < coefficients.length; j++){
-                if (coefficients[j] == 0) continue;
+        for (int i = 0; i < p.coefficients.length; i++) {
+            if (p.coefficients[i] == 0)
+                continue;
+            for (int j = 0; j < coefficients.length; j++) {
+                if (coefficients[j] == 0)
+                    continue;
                 int deg = p.exponents[i] + exponents[j];
                 boolean check = true;
-                for (int k = 0; k < len; k++){
-                    if (arr2[k] == deg){
+                for (int k = 0; k < len; k++) {
+                    if (arr2[k] == deg) {
                         arr[k] += p.coefficients[i] * coefficients[j];
                         check = false;
                     }
@@ -106,27 +167,30 @@ public class Polynomial{
         }
 
         count = 0;
-        for (int i = 0; i < len; i++){
-            if (arr[i] != 0 && arr2[i] != 0){
+        for (int i = 0; i < len; i++) {
+            if (arr[i] != 0 && arr2[i] != 0) {
                 count++;
             }
         }
-        int q_len = count+1;
+        int q_len = count + 1;
         double[] q_arr = new double[q_len];
         int[] q_arr2 = new int[q_len];
 
-        for (int i = 0; i < len; i++){
-            if (arr2[i] == 0){
-                if (arr[i] != 0){
-                    q_arr[q_len-1] = arr[i];
+        for (int i = 0; i < len; i++) {
+            if (arr2[i] == 0) {
+                if (arr[i] != 0) {
+                    q_arr[q_len - 1] = arr[i];
+                } else {
+                    q_arr = new double[q_len - 1];
+                    q_arr2 = new int[q_len - 1];
                 }
                 break;
             }
         }
-        
+
         count = 0;
-        for (int i = 0; i < len; i++){
-            if (arr[i] != 0 && arr2[i] != 0){
+        for (int i = 0; i < len; i++) {
+            if (arr[i] != 0 && arr2[i] != 0) {
                 q_arr[count] = arr[i];
                 q_arr2[count] = arr2[i];
                 count++;
@@ -138,20 +202,20 @@ public class Polynomial{
         return q;
     }
 
-    public void saveToFile(String fileName) throws IOException{
+    public void saveToFile(String fileName) throws IOException {
         FileWriter fw = new FileWriter(fileName);
         String s = "";
-        for(int i = 0; i < coefficients.length; i++){
+        for (int i = 0; i < coefficients.length; i++) {
             s += coefficients[i];
-            if (exponents[i] != 0){
+            if (exponents[i] != 0) {
                 s += "x";
                 s += exponents[i];
                 s += "+";
             }
         }
-        
+
         s = s.replace("+-", "-");
-        s=s.substring(0, s.length()-1);
+        s = s.substring(0, s.length() - 1);
         fw.write(s);
         fw.close();
     }
